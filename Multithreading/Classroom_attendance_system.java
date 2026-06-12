788b67080d1bf444bda18ba1a6bdee8d34be3ee1
debasisswain2007-glitch <debasis.swain2007@gmail.com>
2026-06-12 10:19:22 +0530

    Update Classroom_attendance_system.java 25BEEC02_Debasis Swain

--- Multithreading/Classroom_attendance_system.java
+++ Multithreading/Classroom_attendance_system.java
@@ -89,3 +89,72 @@ Main Method
 Thread Coordination
 Synchronization
 */
+class Classroom {
+    private boolean attendanceStarted = false;
+
+    synchronized void waitForAttendance() {
+        while (!attendanceStarted) {
+            System.out.println(Thread.currentThread().getName() + ": Student Waiting...");
+            try { wait(); } catch (InterruptedException e) { Thread.currentThread().interrupt(); }
+        }
+        System.out.println(Thread.currentThread().getName() + ": Student Marked Present");
+    }
+
+    synchronized void startAttendance() {
+        attendanceStarted = true;
+        System.out.println("Teacher Started Attendance");
+        notifyAll();
+    }
+}
+
+class StudentThread extends Thread {
+    private Classroom classroom;
+
+    StudentThread(Classroom classroom, String name) {
+        super(name);
+        this.classroom = classroom;
+    }
+
+    public void run() {
+        classroom.waitForAttendance();
+    }
+}
+
+class TeacherThread extends Thread {
+    private Classroom classroom;
+
+    TeacherThread(Classroom classroom) {
+        this.classroom = classroom;
+    }
+
+    public void run() {
+        classroom.startAttendance();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Classroom classroom = new Classroom();
+
+        // Create and start multiple student threads
+        StudentThread[] students = new StudentThread[3];
+        for (int i = 0; i < 3; i++) {
+            students[i] = new StudentThread(classroom, "Student-" + (i + 1));
+            students[i].start();
+        }
+
+        Thread.sleep(2000);  // Students wait before teacher starts
+
+        // Start teacher thread
+        TeacherThread teacher = new TeacherThread(classroom);
+        teacher.start();
+
+        // Wait for all threads to complete
+        for (StudentThread student : students) {
+            student.join();
+        }
+        teacher.join();
+
+        System.out.println("Attendance completed!");
+    }
+}
