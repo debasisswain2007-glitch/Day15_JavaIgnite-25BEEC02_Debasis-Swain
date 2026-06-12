418163c475b743b21ab81bb1d632c2185996b1a4
debasisswain2007-glitch <debasis.swain2007@gmail.com>
2026-06-12 10:22:57 +0530

    Update TicketBookingSystem.java 25BEEC02_Debasis Swain

--- Multithreading/TicketBookingSystem.java
+++ Multithreading/TicketBookingSystem.java
@@ -79,3 +79,51 @@ Possible Output
 
     Thread3 Booking Failed
 */
+class Theater {
+    private int totalSeats = 5;
+
+    synchronized void bookSeat(int seats) {
+        if (totalSeats >= seats) {
+            System.out.println(Thread.currentThread().getName() + " Booking Successful");
+            totalSeats -= seats;
+            System.out.println("Remaining Seats = " + totalSeats);
+        } else {
+            System.out.println(Thread.currentThread().getName() + " Booking Failed");
+        }
+    }
+}
+
+class BookingThread extends Thread {
+    private Theater theater;
+    private int seatsRequired;
+
+    BookingThread(Theater theater, int seatsRequired, String name) {
+        super(name);
+        this.theater = theater;
+        this.seatsRequired = seatsRequired;
+    }
+
+    public void run() {
+        theater.bookSeat(seatsRequired);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Theater theater = new Theater();
+
+        BookingThread t1 = new BookingThread(theater, 2, "Thread1");
+        BookingThread t2 = new BookingThread(theater, 2, "Thread2");
+        BookingThread t3 = new BookingThread(theater, 2, "Thread3");
+
+        t1.start();
+        t2.start();
+        t3.start();
+
+        t1.join();
+        t2.join();
+        t3.join();
+
+        System.out.println("\nBooking simulation completed!");
+    }
+}
